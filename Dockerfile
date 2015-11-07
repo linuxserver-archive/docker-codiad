b9109cceae40cb365aa07ea9cab143fc1ba5ba60
smdion <smdion@gmail.com>
2015-11-07 11:08:08 -0600

    first up

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM linuxserver/baseimage.apache
+MAINTAINER smdion <me@seandion.com>
+
+ENV APTLIST="git expect"
+
+# install main packages
+RUN apt-get update -q && \
+apt-get install $APTLIST -qy && \
+
+# cleanup
+apt-get clean -y && \
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# add some files 
+ADD defaults/ /defaults/
+ADD init/ /etc/my_init.d/
+RUN chmod -v +x /etc/service/*/run /etc/my_init.d/*.sh
+
+# ports and volumes
+EXPOSE 80 443
+VOLUME /config
---
+++ init/50_git_update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#base program
+[[ ! -d "/config/www/.git" ]] && (git clone https://github.com/Codiad/Codiad /config/www && cp /defaults/config.php /config/www/config.php)
+cd /config/www
+git pull
+
+#collaborative plugin
+[[ ! -d "/config/www/plugins/Codiad-Collaborative-master/.git" ]] && (git clone https://github.com/Codiad/Codiad-Collaborative /config/www/plugins/Codiad-Collaborative-master/)
+cd /config/www/plugins/Codiad-Collaborative-master/
+git pull
+
+#github plugin
+[[ ! -d "/config/www/plugins/Codiad-CodeGit-master/.git" ]] && (git clone https://github.com/Andr3as/Codiad-CodeGit /config/www/plugins/Codiad-CodeGit-master/)
+cd /config/www/plugins/Codiad-CodeGit-master/
+git pull
+
+#drag and drop plugin
+[[ ! -d "/config/www/plugins/Codiad-DragDrop-master/.git" ]] && (git clone https://github.com/Andr3as/Codiad-DragDrop /config/www/plugins/Codiad-DragDrop-master/)
+cd /config/www/plugins/Codiad-DragDrop-master/
+git pull
+
+#terminal plugin
+[[ ! -d "/config/www/plugins/Codiad-Terminal-master/.git" ]] && (git clone https://github.com/Fluidbyte/Codiad-Terminal /config/www/plugins/Codiad-Terminal-master/)
+cd /config/www/plugins/Codiad-Terminal-master/
+git pull
+
+#fix ownership
+chown -R abc:abc /config
